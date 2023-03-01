5905eb86e1f0cddf79d19685cb36689ccae45201
Stoyan Petrov <stoyan@petrov.ca>
2023-03-01 13:16:28 -0800

    Restored root element into index.html and moved onMount and onClose above the widget creation

--- src/App.jsx
+++ src/App.jsx
@@ -4,80 +4,68 @@ import "@forgerock/login-widget/widget.css";
 import Widget, { modal, journey, user } from "@forgerock/login-widget/modal";
 import { useEffect, useState } from "react";
 
+modal.onMount((dialogElement, formElement) => {
+  console.log("modal.onMount...: " + dialogElement);
+  console.log("modal.onMount...: " + formElement);
+});
+
+modal.onClose(({ reason }) => {
+  console.log("modal.onClose...: " + reason);
+});
+
+new Widget({
+  target: document.getElementById("widget-root"),
+  props: {
+    config: {
+      serverConfig: {
+        baseUrl: "https://openam-forgerrock-sdksteanant.forgeblocks.com/am/",
+        timeout: 5000,
+      },
+      clientId: "widget-client",
+      redirectUri: "https://sdkapp.example.com:8443/",
+      realmPath: "alpha",
+      scope: "openid profile email address phone",
+      // tree: "WebAuthn",
+      // tokenStore: "indexedDB",
+    },
+    links: {
+      termsAndConditions: "terms",
+      // termsAndConditionsLinkText: "Gu-gu gaa",
+    },
+    style: {
+      checksAndRadios: "animated", // OPTIONAL; choices are 'animated' or 'standard'
+      labels: "stacked", // OPTIONAL; choices are 'floating' or 'stacked'
+    },
+  },
+});
+
 function App() {
   const [userInfo, setUserInfo] = useState(null);
-  const [count, setCount] = useState(0);
-  // const [isAuthorized, setIsAuthorized] = useState(false);
+
+  journey.onSuccess(
+    (data) => {
+      setUserInfo(data.user.response);
+    },
+    [setUserInfo]
+  );
+
+  journey.onFailure((data) => {
+    console.log("Journey failed: " + data.journey.error);
+  });
+
+  useEffect(() => {
+    async function fetchData() {
+      const authorized = await user.authorized(false);
+      console.log("user.authorized(): " + authorized);
+    }
+    fetchData();
+  }, [userInfo, setUserInfo]);
 
   let url = new URL(window.location.href);
   let codeParam = url.searchParams.get("code");
   let stateParam = url.searchParams.get("state");
   let formPostEntryParam = url.searchParams.get("form_post_entry");
-
   let suspendedId = url.searchParams.get("suspendedId");
-
-  console.log("suspendedId: " + suspendedId);
-
-  useEffect(() => {
-    journey.onSuccess((data) => {
-      setCount(count + 1);
-      setUserInfo(data.user.response);
-    });
-  }, [count, setCount, setUserInfo]);
-
-  useEffect(() => {
-    journey.onFailure((data) => {
-      console.log("Journey failed: " + data.journey.error);
-    });
-  }, [setUserInfo]);
-
-  useEffect(() => {
-    new Widget({
-      target: document.getElementById("widget-root"),
-      props: {
-        config: {
-          serverConfig: {
-            baseUrl:
-              "https://openam-forgerrock-sdksteanant.forgeblocks.com/am/",
-            timeout: 5000,
-          },
-          clientId: "widget-client",
-          redirectUri: "https://sdkapp.example.com:8443/",
-          realmPath: "alpha",
-          scope: "openid profile email address phone",
-          // tree: "WebAuthn",
-          // tokenStore: "indexedDB",
-        },
-        links: {
-          termsAndConditions: "terms",
-          // termsAndConditionsLinkText: "Gu-gu gaa",
-        },
-        style: {
-          checksAndRadios: "animated", // OPTIONAL; choices are 'animated' or 'standard'
-          labels: "stacked", // OPTIONAL; choices are 'floating' or 'stacked'
-        },
-      },
-    });
-
-    modal.onMount((dialogElement, formElement) => {
-      console.log("modal.onMount...: " + dialogElement);
-      console.log("modal.onMount...: " + formElement);
-    });
-
-    modal.onClose(({ reason }) => {
-      console.log("modal.onClose...: " + reason);
-    });
-  }, []);
-
-  useEffect(() => {
-    async function fetchData() {
-      const authorized = await user.authorized();
-      console.log(
-        "user.authorized(): " + JSON.stringify(authorized, null, " ")
-      );
-    }
-    fetchData();
-  }, [userInfo, setUserInfo]);
 
   useEffect(() => {
     /**
@@ -91,7 +79,9 @@ function App() {
 
     if (suspendedId) {
       modal.open({
-        resumeUrl: window.location.href,
+        resumeUrl:
+          "https://sdkaap.example.com:8443/ResetPassword?suspendedId=" +
+          suspendedId,
       });
     }
 
@@ -112,7 +102,6 @@ function App() {
 
   return (
     <div className="App">
-      <div id="widget-root"></div>
       <header className="App-main">
         <h4>ForgeRock Login Widget :: Modal Demo</h4>
         <hr />
@@ -133,9 +122,7 @@ function App() {
             <button
               onClick={() => {
                 // modal.open({ journey: "SocialLogin-web" });
-                // modal.open({ journey: "SocialLogin-web" });
-
-                modal.open({ journey: "ResetPassword" });
+                modal.open({ journey: "SocialLogin-web" });
               }}
             >
               Login
